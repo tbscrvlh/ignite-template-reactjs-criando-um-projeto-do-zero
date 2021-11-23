a77f5e6ba728a0b9afdd90d8d0f745006c052243
Tobias Carvalho <tobias.jung.carvalho@gmail.com>
2021-11-23 12:09:28 -0300

    desafio complementar

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -11,9 +11,12 @@ import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import Link from 'next/link';
+import Comments from '../../components/Comments';
 
 interface Post {
   first_publication_date: string | null;
+  last_publication_date: string | null;
   data: {
     title: string;
     banner: {
@@ -31,9 +34,24 @@ interface Post {
 
 interface PostProps {
   post: Post;
+  navigation: {
+    prevPost: {
+      uid: string;
+      data: {
+        title: string;
+      }
+    }[];
+    nextPost: {
+      uid: string;
+      data: {
+        title: string;
+      }
+    }[];
+  };
+  preview: boolean;
 }
 
-export default function Post({ post }: PostProps): JSX.Element {
+export default function Post({ post, preview, navigation }: PostProps): JSX.Element {
 
   const totalWords = post.data.content.reduce((total, contentItem) => {
     total += contentItem.heading.split(' ').length;
@@ -57,6 +75,18 @@ export default function Post({ post }: PostProps): JSX.Element {
     }
   )
 
+  const isPostEdited = post.first_publication_date !== post.last_publication_date;
+  let editionDate;
+  if (isPostEdited) {
+    editionDate = format(
+      new Date(post.last_publication_date),
+      "'editado em' dd MMM yyyy', às' H':'m",
+      {
+        locale: ptBR,
+      }
+    )
+  }
+
   return (
     <>
       <Head>
@@ -69,33 +99,62 @@ export default function Post({ post }: PostProps): JSX.Element {
           <div className={styles.postTop}>
             <h1>{post.data.title}</h1>
             <ul>
-                <li>
-                  <FiCalendar />
-                  {formattedDate}
-                </li>
-                <li>
-                  <FiUser />
-                  {post.data.author}
-                </li>
-                <li>
-                  <FiClock />
-                  {`${readTime} min`}
-                </li>
-              </ul>
+              <li>
+                <FiCalendar />
+                {formattedDate}
+              </li>
+              <li>
+                <FiUser />
+                {post.data.author}
+              </li>
+              <li>
+                <FiClock />
+                {`${readTime} min`}
+              </li>
+            </ul>
+            {isPostEdited && <span>{editionDate}</span>}
           </div>
 
           {post.data.content.map(content => {
             return (
               <article key={content.heading}>
                 <h2>{content.heading}</h2>
-                <div 
+                <div
                   className={styles.postContent}
-                  dangerouslySetInnerHTML={{ __html: RichText.asHtml(content.body)}}
+                  dangerouslySetInnerHTML={{ __html: RichText.asHtml(content.body) }}
                 />
               </article>
             )
           })}
         </div>
+        <section className={`${styles.navigation} ${commonStyles.container}`}>
+          {navigation?.prevPost.length > 0 && (
+            <div>
+              <h3>{navigation.prevPost[0].data.title}</h3>
+              <Link href={`/posts/${navigation.prevPost[0].uid}`}>
+                <a>Post anterior</a>
+              </Link>
+            </div>
+          )}
+          {navigation?.nextPost.length > 0 && (
+            <div>
+              <h3>{navigation.nextPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.nextPost[0].uid}`}>
+                <a>Próximo post</a>
+              </Link>
+            </div>
+          )}
+        </section>
+
+        <Comments />
+
+        {preview && (
+          <aside>
+            <Link href="/api/exit-preview">
+              <a className={commonStyles.preview}>Sair do modo Preview</a>
+            </Link>
+          </aside>
+        )}
       </main>
     </>
   )
@@ -121,33 +180,63 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 };
 
-export const getStaticProps: GetStaticProps = async context => {
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData
+}) => {
   const prismic = getPrismicClient();
-  const { slug } = context.params;
-  const response = await prismic.getByUID('posts', String(slug), {});
+  const { slug } = params;
+  const response = await prismic.getByUID('posts', String(slug), {
+    ref: previewData?.ref || null,
+  });
+
+  const prevPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]'
+    }
+  )
+
+  const nextPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.last_publication_date desc]'
+    }
+  )
 
   const post = {
     uid: response.uid,
     first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     data: {
-    title: response.data.title,
-    subtitle: response.data.subtitle,
-    author: response.data.author,
-    banner: {
-      url: response.data.banner.url,
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
+      banner: {
+        url: response.data.banner.url,
+      },
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: [...content.body],
+        }
+      }),
     },
-    content: response.data.content.map(content => {
-      return {
-        heading: content.heading,
-        body: [...content.body],
-      }
-    }),
-  },
   }
- 
+
   return {
     props: {
-      post
+      post,
+      navigation: {
+        prevPost: prevPost?.results,
+        nextPost: nextPost?.results
+      },
+      preview
     }
   }
 };
